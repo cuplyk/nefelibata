c463c437871ff25f689216bbe4080a8d39d80474
cuplyk <121808859+cuplyk@users.noreply.github.com>
2024-03-27 20:58:32 +0000

    add Post class

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,37 @@
-from django.db import models
-
 # Create your models here.
+from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+
+STATUS_CHOICES = (
+    (0, "Draft"),
+    (1, "Published")
+)
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=300, unique=True)
+    slug = models.SlugField(max_length=300, unique=True, editable=False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='blog_posts')
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS_CHOICES, default=0)
+    tags = models.ManyToManyField(Tag, related_name='posts')
+    image = models.ImageField(upload_to='post_images/', null=True, blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_on']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
