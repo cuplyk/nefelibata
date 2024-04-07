77735366283be8c4624119f2c133c7c9a82db29c
cuplyk <121808859+cuplyk@users.noreply.github.com>
2024-04-07 23:07:55 +0000

    change files path

--- django_main_project/settings.py
+++ django_main_project/settings.py
@@ -120,8 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+MEDIA_URL = '/media_upload/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_upload/')
 STATIC_URL = "/static/"
 
 
