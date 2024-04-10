21711f2da3be0cc0229717561908e2f8edcfee40
cuplyk <121808859+cuplyk@users.noreply.github.com>
2024-04-10 21:58:57 +0000

    urls test

--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ from .views import IndexFirstListView, image_test_view
 from django.conf.urls.static import static
 
 urlpatterns = [
+    #prova
     path("", IndexFirstListView.as_view(), name='home.html' ),
     path('image-test/', image_test_view, name='image_test'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
