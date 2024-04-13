2465a5cba50d19593f99fc433d507cd6c62ac75d
cuplyk <121808859+cuplyk@users.noreply.github.com>
2024-04-13 18:12:26 +0000

    fixed add image

--- django_main_project/urls.py
+++ django_main_project/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -25,3 +27,7 @@ urlpatterns = [
     re_path(r'mdeditor/', include('mdeditor.urls'))
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
