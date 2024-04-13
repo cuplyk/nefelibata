d3553fdba295ca992b30bae88585269447fcb085
cuplyk <121808859+cuplyk@users.noreply.github.com>
2024-04-13 19:47:17 +0000

    add article_detail file

--- blog/urls.py
+++ blog/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from django.conf import settings
 
-from .views import IndexFirstListView, image_test_view
+from .views import IndexFirstListView, image_test_view, ArticleDetailViws
 from django.conf.urls.static import static
 
 urlpatterns = [
     #prova
     path("", IndexFirstListView.as_view(), name='home.html' ),
     path('image-test/', image_test_view, name='image_test'),
+    path('arcticle/<slug:slug>/', ArticleDetailViws.as_view(), name='article_detail')
     
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,19 +1,43 @@
+from typing import Any
+from django.db.models.base import Model as Model
+from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
+from django.shortcuts import get_list_or_404
 
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DeleteView
 # Create your views here.
 
 
 class IndexFirstListView(TemplateView):
-    template_name = 'home.html'
+    template_name = 'blog/blog_index.html'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['posts'] = Post.objects.order_by('-created_on')[:4]
+        
         return context
     
+
+
+class ArticleDetailViws(DeleteView):
+    model = Post
+    template_name = 'blog/articles/article_detail.html'
+    context_object_name = 'article'
+    slug_field = 'slug'
+    slug_url_kwarg = 'slug'
+
+    def get_object(self, queryset=None):
+        slug = self.kwargs.get('slug')
+
+        return get_list_or_404(Post, slug=slug)
+
+
+
+
+
+
 def image_test_view(request):
     posts = Post.objects.all()
     return render(request, 'image_test.html', {'posts': posts})
