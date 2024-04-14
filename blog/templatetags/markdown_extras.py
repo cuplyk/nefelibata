a1772372b8995f9f15d838a3adb4c872260b314c
cuplyk <121808859+cuplyk@users.noreply.github.com>
2024-04-14 21:31:31 +0000

    modifications for markdown

--- blog/templatetags/markdown_extras.py
+++ blog/templatetags/markdown_extras.py
@@ -1,4 +1,4 @@
-from django import template
+"""from django import template
 from markdown2 import markdown
 
 register = template.Library()
@@ -7,3 +7,18 @@ register = template.Library()
 def render_markdown(content):
   return markdown(content)
 
+"""
+
+from django import template
+from django.utils.safestring import mark_safe
+import markdown2
+register = template.Library()
+
+@register.filter
+def render_markdown(content):
+    # Convert Markdown content to HTML
+    html_content = markdown2.markdown(content)
+
+    # Apply code syntax highlighting with highlight.js
+    html_content = '<div class="markdown">' + html_content + '</div>'
+    return mark_safe(html_content)
